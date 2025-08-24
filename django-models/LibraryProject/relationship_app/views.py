ab23a59cee28c98d4659501ccaaea139587ccd1e
AbuHumayd1 <daarussalaam22@gmail.com>
2025-08-24 18:09:42 +0100

    View fixed again

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView  
 from .models import Book
-from .models import Library   # ✅ explicit import so the check finds it
+from .models import Library   
 
 
 # Function-based view for listing all books
