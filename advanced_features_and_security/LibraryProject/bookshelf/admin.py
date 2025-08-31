8abdf698efd9b69516e1435cff60f132a65a20f2
AbuHumayd1 <daarussalaam22@gmail.com>
2025-08-31 20:32:47 +0100

    Custom User registered in admin

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
@@ -11,3 +13,16 @@ class BookAdmin(admin.ModelAdmin):
 
     # Add a search box at the top
     search_fields = ("title", "author")
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    # Add our custom fields to the admin forms
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+# ✅ Register the CustomUser model with the custom admin class
+admin.site.register(CustomUser, CustomUserAdmin)
