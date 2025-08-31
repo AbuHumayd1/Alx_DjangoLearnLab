02be650af7969ed30c4d5d30a8803a2c3a1dd5cd
AbuHumayd1 <daarussalaam22@gmail.com>
2025-08-31 14:25:37 +0100

    Done

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -17,6 +17,9 @@ def register(request):
 
 
 
+
+
+
 def is_admin(user):
     return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
 
@@ -27,6 +30,7 @@ def admin_view(request):
 def is_librarian(user):
     return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
 
+# View restricted to Librarians
 @user_passes_test(is_librarian)
 def librarian_view(request):
     return render(request, 'librarian_view.html')
@@ -34,6 +38,7 @@ def librarian_view(request):
 def is_member(user):
     return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
 
+# View restricted to Members
 @user_passes_test(is_member)
 def member_view(request):
     return render(request, 'member_view.html')
